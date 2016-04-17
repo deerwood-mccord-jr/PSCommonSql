1c5cf40d41331ced114ee9084306e9f5b5cd872a
Chris Duck <gpduck@whatsupduck.net>
2016-04-16 21:54:45 -0500

    Rename module

---
+++ PSCommonSql/PSCommonSql.psm1
@@ -0,0 +1,29 @@
+﻿#handle PS2
+    if(-not $PSScriptRoot)
+    {
+        $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+    }
+
+#Get public and private function definition files.
+    $Public  = Get-ChildItem $PSScriptRoot\*.ps1 -ErrorAction SilentlyContinue
+    #$Private = Get-ChildItem $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue 
+
+#Dot source the files
+    Foreach($import in @($Public))
+    {
+        Try
+        {
+            #PS2 compatibility
+            if($import.fullname)
+            {
+                . $import.fullname
+            }
+        }
+        Catch
+        {
+            Write-Error "Failed to import function $($import.fullname): $_"
+        }
+    }
+    
+#Create some aliases, export public functions
+    Export-ModuleMember -Function $($Public | Select -ExpandProperty BaseName)
