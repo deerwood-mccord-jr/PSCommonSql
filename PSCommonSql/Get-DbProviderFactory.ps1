694467902c3a68acda8be156a0e126397940ac17
Chris Duck <chris.duck@vizientinc.com>
2016-08-31 20:03:31 -0500

    Tries ::Instance when default constructor fails

--- PSCommonSql/Get-DbProviderFactory.ps1
+++ PSCommonSql/Get-DbProviderFactory.ps1
@@ -10,8 +10,18 @@ function Get-DbProviderFactory {
     $FactoryDefinition = $Script:DbProviderFactories[$DbProviderName]
     if($FactoryDefinition) {
       #Use our own provider registry as DbProviderFactories only reads the configuration the first time it is called
-      Write-Debug "Creating instance of $($FactoryDefinition.Type)" 
-      New-Object $FactoryDefinition.Type
+      Write-Debug "Creating instance of $($FactoryDefinition.Type)"
+      try {
+        New-Object $FactoryDefinition.Type
+      } catch {
+        try {
+          $Type = [Type]$FactoryDefinition.Type
+          $Type::Instance
+        } catch {
+          throw (New-Object System.ArgumentException("Unable to create an instance of the requested type ($($FactoryDefinition.Type))"))
+        }
+      }
+      
     } else {
       #Try the system registered providers
       try {
