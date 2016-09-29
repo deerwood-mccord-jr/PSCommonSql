b186e394255d6625f6709c99b98b9ec1731d5bbe
Chris Duck <gpduck@whatsupduck.net>
2016-09-29 12:51:36 -0500

    Fixes searching the machine.config for providers.

--- PSCommonSql/Get-DbProviderFactory.ps1
+++ PSCommonSql/Get-DbProviderFactory.ps1
@@ -8,29 +8,28 @@ function Get-DbProviderFactory {
   )
   if($Script:DbProviderFactories) {
     $FactoryDefinition = $Script:DbProviderFactories[$DbProviderName]
-    if($FactoryDefinition) {
-      #Use our own provider registry as DbProviderFactories only reads the configuration the first time it is called
-      Write-Debug "Creating instance of $($FactoryDefinition.Type)"
+  } else {
+    $FactoryDefinition = $null
+  }
+  if($FactoryDefinition) {
+    #Use our own provider registry as DbProviderFactories only reads the configuration the first time it is called
+    Write-Debug "Creating instance of $($FactoryDefinition.Type)"
+    try {
+      New-Object $FactoryDefinition.Type
+    } catch {
       try {
-        New-Object $FactoryDefinition.Type
+        $Type = [Type]$FactoryDefinition.Type
+        $Type::Instance
       } catch {
-        try {
-          $Type = [Type]$FactoryDefinition.Type
-          $Type::Instance
-        } catch {
-          throw (New-Object System.ArgumentException("Unable to create an instance of the requested type ($($FactoryDefinition.Type))"))
-        }
-      }
-      
-    } else {
-      #Try the system registered providers
-      try {
-        [System.Data.Common.DbProviderFactories]::GetFactory($DbProviderName)
-      } catch [System.ArgumentException] {
-        throw (New-Object System.ArgumentException("Unable to find the requested .Net Framework Data Provider. Use 'Register-DbProvider' to register one."))
+        throw (New-Object System.ArgumentException("Unable to create an instance of the requested type ($($FactoryDefinition.Type))"))
       }
     }
   } else {
-    throw (New-Object System.ArgumentException("Unable to find the requested .Net Framework Data Provider. Use 'Register-DbProvider' to register one."))
+    #Try the system registered providers
+    try {
+      [System.Data.Common.DbProviderFactories]::GetFactory($DbProviderName)
+    } catch [System.ArgumentException] {
+      throw (New-Object System.ArgumentException("Unable to find the requested .Net Framework Data Provider. Use 'Register-DbProvider' to register one."))
+    }
   }
 }
